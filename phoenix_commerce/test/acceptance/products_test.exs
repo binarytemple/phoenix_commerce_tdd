6c73bdf8b5a70a79f62554505f497a4e962003b5
bryan hunt <github@binarytemple.co.uk>
2016-04-07 09:28:26 +0100

    added products view and test

--- phoenix_commerce/test/acceptance/products_test.exs
+++ phoenix_commerce/test/acceptance/products_test.exs
@@ -3,16 +3,36 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
   use Hound.Helpers
   use ExUnit.Case
   hound_session
+  
+  alias PhoenixCommerce.Product
+  alias PhoenixCommerce.Repo
 
     test "/products has a list of products" do 
-    navigate_to "/products"
-    
-
-
-    assert find_element(:css,"ul.products")
+      navigate_to "/products"
+      assert find_element(:css,"ul.products")
     end 
 
+    test "/products - product details include name, description and price" do 
+          {:ok, product} = 
+          %Product{ 
+          name: "Some product",
+          description: "Some product description",
+          price: Decimal.new("25.50")
+          } |> Repo.insert
 
+      navigate_to "/products"
+
+      #IO.inspect product_li
+      product_li = find_element(:css,"ul.products li")
+      name = find_within_element(product_li, :css, "h3")
+      price = find_within_element(product_li, :css, "h4")
+      description = find_within_element(product_li, :css, "p")
+
+      assert visible_text(name) == product.name 
+      assert visible_text(price) == product.price
+      assert visible_text(description) == product.description
+
+    end 
 
 end
 
